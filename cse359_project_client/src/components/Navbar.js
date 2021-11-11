fcd4eededb49c7b2d7a18edb8952083c8e93af96
[Jeff] <[jeffyche@gmail.com]>
2021-11-11 16:30:06 -0600

    basic file setup

---
+++ cse359_project_client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    
+    render() {
+        return (
+            <p>Need to implement a navbar that allows movement between pages.</p>
+        )
+    }
+}
+
+export default Navbar;
--- cse359_project_client/src/index.js
+++ cse359_project_client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// pages
+import home from './pages/home';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

468a1d25041cd8224e10f1e09c4fc0c316d735ef
[Jeff] <[jeffyche@gmail.com]>
2021-11-11 16:32:11 -0600

    small edit

--- cse359_project_client/src/index.js
+++ cse359_project_client/src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// pages
-import home from './pages/home';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
